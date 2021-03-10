e0630c2594f7fc212981faa8f02ed2714a3d37d4
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2021-03-10 20:17:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,17 +11,32 @@ document.querySelector("#dd").innerHTML = d.getUTCDate();
 document.querySelector("#mm").innerHTML = d.getMonth();
 document.querySelector("#yyyy").innerHTML = d.getFullYear();
 
-api = 'https://api.exchangeratesapi.io/latest?base=';
+//const answer = async () => {}
+api = `https://api.exchangeratesapi.io/latest?base=${from_currency.value}`;
 fetch(api)
 .then(Response => Response.json())
-.then(content => console.log(content.rates.INR));
+.then((content) => {
+    document.querySelector("#according").innerHTML = content.date;
+    console.log(content.rates);
+});
 
+const base_to_final = (base_curr,to_curr) => {
+    base_curr = from_ammount.value;
+    to_curr = to_ammount.value;
+    
+    var result = base_curr*to_curr;
+
+    return result;
+}
 
 const cnv = document.querySelector("#convert");
 
-cnv.addEventListener("click", () =>{
+cnv.addEventListener("click", () => {
     var base_curr = from_ammount.value;
-    var result = to_ammount.value;
-
+    var to_curr = to_ammount.value;
+    
+    var result = base_curr*to_curr;
+    //result = `content.rates.${to_currency.value}`;
+    //document.querySelector("#curr_amount_2").innerHTML;
 });
 
